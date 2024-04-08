9f013796c05c4aacec25eb84840ce2714ee72b81
GAL_son <pancerek1@gmail.com>
2024-04-08 11:04:40 +0200

    Basic page serving

--- api/lib/index.ts
+++ api/lib/index.ts
@@ -1,5 +1,9 @@
+import IndexController from './controllers/index.controller';
 import App from './app';
 
-const app: App = new App();
+
+const app: App = new App([
+    new IndexController()
+]);
 
 app.listen();
