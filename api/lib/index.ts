7e9064e54bae2c2aa038f7e2ac27d84be0c7766a
GAL_son <pancerek1@gmail.com>
2024-04-25 19:02:22 +0200

    User controller

--- api/lib/index.ts
+++ api/lib/index.ts
@@ -2,10 +2,12 @@ import App from './app';
 
 import IndexController from './controllers/index.controller';
 import DataController from './controllers/data.controller';
+import UserController from './controllers/user.controller';
 
 const app: App = new App([
     new IndexController(),
-    new DataController()
+    new DataController(),
+    new UserController(),
 ]);
 
 app.listen();
