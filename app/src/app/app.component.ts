307ea9cabc6a151b466e4729ac5f199a7efed2a4
GAL_son <pancerek1@gmail.com>
2024-05-16 18:00:07 +0200

    Components generation

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { BlogComponent } from "./components/blog/blog.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, BlogComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'app';
+    public counter: number = 0;
+
+    add() { this.counter++; }
+    remove() { this.counter--; }
 }
---
+++ app/src/app/components/blog-item/blog-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'item',
+  standalone: true,
+  imports: [],
+  templateUrl: './blog-item.component.html',
+  styleUrl: './blog-item.component.css'
+})
+export class BlogItemComponent {
+
+}
