1f0e93c7bdf89bd163e6c9db22da5a6511836181
GAL_son <pancerek1@gmail.com>
2024-06-12 12:35:43 +0200

    Landing page

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -2,10 +2,15 @@ import { Routes } from '@angular/router';
 import { BlogComponent } from './components/blog/blog.component';
 import { BlogItemDetailsComponent } from './components/blog-item-details/blog-item-details.component';
 import { BlogHomeComponent } from './components/blog-home/blog-home.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
     {
         path: '',
+        component: HomeComponent,
+    },
+    {
+        path: 'blog',
         component: BlogHomeComponent,
     },
     {
